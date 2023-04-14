13d3a61ee800190d2901817183cb04c65b77ce71
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-04-13 23:25:07 -0700

    Update visual_odometry.py

--- ratslam/visual_odometry.py
+++ ratslam/visual_odometry.py
@@ -53,7 +53,7 @@ class VisualOdometry(object):
         :return: the deslocation and rotation of the image from the previous 
                  frame as a 2D tuple of floats.
         '''
-        print("hi " + type(img))
+        print("hi " + str(type(img)))
         subimg = img
         template = self._create_template(subimg)
 
