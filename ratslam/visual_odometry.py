5127e1e6089d3dc5af6d3b698f76d22a51db5977
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-04-13 23:27:54 -0700

    Update visual_odometry.py

--- ratslam/visual_odometry.py
+++ ratslam/visual_odometry.py
@@ -54,7 +54,7 @@ class VisualOdometry(object):
                  frame as a 2D tuple of floats.
         '''
         print("hi " + str(img.shape))
-        subimg = img[:,:1080]
+        subimg = img[:,:475]
         template = self._create_template(subimg)
 
         # VTRANS
